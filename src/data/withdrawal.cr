55e885ba17c8cf1d6e4f53e14c914c26a52aa36c
Jonathan B <greenbigfrog@gmail.com>
2019-10-23 21:30:48 +0200

    Fix bigint/int confusion and rename user_id to account_id

--- src/data/withdrawal.cr
+++ src/data/withdrawal.cr
@@ -3,15 +3,15 @@ struct TB::Data::Withdrawal
     id: Int32,
     pending: Bool,
     coin: Int32,
-    user_id: Int64,
+    account_id: Int32,
     address: String,
     amount: BigDecimal,
-    transaction: Int64,
+    transaction: Int32,
     created_time: Time
   )
 
-  def self.create(coin : Coin, user_id : Int32, address : String, amount : BigDecimal, transaction : Int32, db : DB::Connection = TB::DATA.connection)
-    db.query_one("INSERT INTO withdrawals(coin, user_id, address, amount, transaction) VALUES ($1, $2, $3, $4, $5) RETURNING id", coin.id, user_id, address, amount, transaction, as: Int32)
+  def self.create(coin : Coin, account_id : Int32, address : String, amount : BigDecimal, transaction : Int32, db : DB::Connection = TB::DATA.connection)
+    db.query_one("INSERT INTO withdrawals(coin, account_id, address, amount, transaction) VALUES ($1, $2, $3, $4, $5) RETURNING id", coin.id, user_id, address, amount, transaction, as: Int32)
   end
 
   def self.read(id : Int32)
