0a42e647ffe077f3926198e43d8b50305aa33efe
Jonathan B <greenbigfrog@gmail.com>
2020-02-04 19:39:31 +0100

    Update URL and move formatting to output

--- src/constants.cr
+++ src/constants.cr
@@ -1,7 +1,7 @@
 module TB
   START_TIME = Time.now
   TERMS      = "In no event shall this bot or its dev be responsible for any loss, theft or misdirection of funds."
-  SUPPORT    = "<https://contact.tipbot.info>"
+  SUPPORT    = "https://contact.cryptobutler.info"
   LOG        = Logger.new(STDOUT)
   DATA       = PG.connect("postgresql://postgres@database:5432/tipbot")
 end
